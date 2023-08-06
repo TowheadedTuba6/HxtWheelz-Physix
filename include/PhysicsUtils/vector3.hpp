ef77c7fa409f7e8061a25b64d7f9f968a7c7484e
Dustin <97925087+HotWheelzCodez@users.noreply.github.com>
2023-08-06 13:36:23 -0700

    Add files via upload

---
+++ include/PhysicsUtils/vector3.hpp
@@ -0,0 +1,48 @@
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
+class Vector3 {
+public:
+  float x;
+  float y;
+  float z;
+
+  Vector3() : x(0.0f), y(0.0f), z(0.0f) { }
+  Vector3(float x, float y, float z) : x(x), y(y), z(z) { }
+
+  Vector3 operator+(const Vector3& other) const {
+    return Vector3(x + other.x, y + other.y, z + other.z);
+  }
+
+  Vector3 operator-(const Vector3& other) const {
+    return Vector3(x - other.x, y - other.y, z - other.z);
+  }
+
+  Vector3 operator*(const Vector3& other) const {
+    return Vector3(x * other.x, y * other.y, z * other.z);
+  }
+
+  void normalize() {
+    float length = sqrt(x * x + y * y + z * z);
+    if (length > 0.0f) {
+      float invLength = 1.0f / length;
+      x *= invLength;
+      y *= invLength;
+      z *= invLength;
+    }
+  }
+
+  float dot(const Vector3& other) const {
+    return x * other.x + y * other.y + z * other.z;
+  }
+
+  Vector3 cross(const Vector3& other) const {
+    return Vector3 (
+      y * other.z - z * other.y,
+      z * other.x - x * other.z,
+      x * other.y - y * other.x
+    );
+  }
+};
+
+#endif /* VECTOR3_H */
